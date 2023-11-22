a4cebf78e44f6cd499dc8aafce418577a9d170ce
Anthony Cruz <anthonyleierlw@gmail.com>
2023-11-22 08:34:37 -0300

    CPF como objeto de valor

--- src/@core/common/domain/value-objects/name.vo.spec.ts
+++ src/@core/common/domain/value-objects/name.vo.spec.ts
@@ -1,15 +1,6 @@
-import { Customer } from 'src/@core/domain/entities/customer.entity';
 import { Name } from './name.vo';
 
 test('Deve criar um nome válido', () => {
   const name = new Name('Anthony');
   expect(name.value).toBe('Anthony');
-
-  const customer = new Customer({
-    cpf: '111.222.333-44',
-    name,
-  });
-
-  // customer.name.value = 'Bruno'; // Não fazer
-  customer.name = new Name('Bruno');
 });
