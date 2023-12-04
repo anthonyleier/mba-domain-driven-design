8abe25ced7539ec8006c55cda6d85e5e1703c587
Anthony Cruz <anthonyleierlw@gmail.com>
2023-12-04 09:56:30 -0300

    Integração com repositórios

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,5 +1,11 @@
+import { EntityManager } from '@mikro-orm/mysql';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { CustomerMysqlRepository } from 'src/@core/events/infra/db/repositories/customer-mysql.repository';
+import { EventMysqlRepository } from 'src/@core/events/infra/db/repositories/event-mysql.repository';
+import { OrderMysqlRepository } from 'src/@core/events/infra/db/repositories/order-mysql.repository';
+import { PartnerMysqlRepository } from 'src/@core/events/infra/db/repositories/partner-mysql.repository';
+import { SpotReservationMysqlRepository } from 'src/@core/events/infra/db/repositories/spot-reservation-mysql.repository';
 import {
   CustomerSchema,
   EventSchema,
@@ -23,6 +29,33 @@ import {
         SpotReservationSchema,
       ],
     }),
+  ],
+  providers: [
+    {
+      provide: 'IPartnerRepository',
+      useFactory: (em: EntityManager) => new PartnerMysqlRepository(em),
+      inject: [EntityManager],
+    },
+    {
+      provide: 'ICustomerRepository',
+      useFactory: (em: EntityManager) => new CustomerMysqlRepository(em),
+      inject: [EntityManager],
+    },
+    {
+      provide: 'IEventRepository',
+      useFactory: (em: EntityManager) => new EventMysqlRepository(em),
+      inject: [EntityManager],
+    },
+    {
+      provide: 'IOrderRepository',
+      useFactory: (em: EntityManager) => new OrderMysqlRepository(em),
+      inject: [EntityManager],
+    },
+    {
+      provide: 'ISpotReservationRepository',
+      useFactory: (em: EntityManager) => new SpotReservationMysqlRepository(em),
+      inject: [EntityManager],
+    },
   ],
 })
 export class EventsModule {}
