e56ef31cde69288e17154260f7cf0dfd9aa809b1
Anthony Cruz <anthonyleierlw@gmail.com>
2023-12-06 14:27:55 -0300

    Integrando eventos e listeners

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,22 +1,5 @@
-import { EntityManager } from '@mikro-orm/mysql';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { Module } from '@nestjs/common';
-import { IUnitOfWork } from 'src/@core/common/application/unit-of-work.interface';
-import { CustomerService } from 'src/@core/events/application/customer.service';
-import { EventService } from 'src/@core/events/application/event.service';
-import { OrderService } from 'src/@core/events/application/order.service';
-import { PartnerService } from 'src/@core/events/application/partner.service';
-import { PaymentGateway } from 'src/@core/events/application/payment.gateway';
-import { ICustomerRepository } from 'src/@core/events/domain/repositories/customer-repository.interface';
-import { IEventRepository } from 'src/@core/events/domain/repositories/event-repository.interface';
-import { IOrderRepository } from 'src/@core/events/domain/repositories/order-repository.interface';
-import { IPartnerRepository } from 'src/@core/events/domain/repositories/partner-repository.interface';
-import { ISpotReservationRepository } from 'src/@core/events/domain/repositories/spot-reservation-repository.interface';
-import { CustomerMysqlRepository } from 'src/@core/events/infra/db/repositories/customer-mysql.repository';
-import { EventMysqlRepository } from 'src/@core/events/infra/db/repositories/event-mysql.repository';
-import { OrderMysqlRepository } from 'src/@core/events/infra/db/repositories/order-mysql.repository';
-import { PartnerMysqlRepository } from 'src/@core/events/infra/db/repositories/partner-mysql.repository';
-import { SpotReservationMysqlRepository } from 'src/@core/events/infra/db/repositories/spot-reservation-mysql.repository';
+import { Module, OnModuleInit } from '@nestjs/common';
 import {
   CustomerSchema,
   EventSchema,
@@ -25,26 +8,50 @@ import {
   OrderSchema,
   PartnerSchema,
   SpotReservationSchema,
-} from 'src/@core/events/infra/db/schemas';
+} from '../@core/events/infra/db/schemas';
+import { PartnerMysqlRepository } from '../@core/events/infra/db/repositories/partner-mysql.repository';
+import { EntityManager } from '@mikro-orm/mysql';
+import { CustomerMysqlRepository } from '../@core/events/infra/db/repositories/customer-mysql.repository';
+import { EventMysqlRepository } from '../@core/events/infra/db/repositories/event-mysql.repository';
+import { OrderMysqlRepository } from '../@core/events/infra/db/repositories/order-mysql.repository';
+import { SpotReservationMysqlRepository } from '../@core/events/infra/db/repositories/spot-reservation-mysql.repository';
+import { PartnerService } from '../@core/events/application/partner.service';
+import { CustomerService } from '../@core/events/application/customer.service';
+import { EventService } from '../@core/events/application/event.service';
+import { OrderService } from '../@core/events/application/order.service';
+import { PaymentGateway } from '../@core/events/application/payment.gateway';
+import { IPartnerRepository } from '../@core/events/domain/repositories/partner-repository.interface';
 import { PartnersController } from './partners/partners.controller';
 import { CustomersController } from './customers/customers.controller';
 import { EventsController } from './events/events.controller';
 import { EventSectionsController } from './events/event-sections.controller';
 import { EventSpotsController } from './events/event-spots.controller';
 import { OrdersController } from './orders/orders.controller';
+import { ApplicationModule } from '../application/application.module';
+import { ApplicationService } from '../@core/common/application/application.service';
+import { DomainEventManager } from '../@core/common/domain/domain-event-manager';
+import { PartnerCreated } from '../@core/events/domain/events/domain-events/partner-created.event';
+import { MyHandlerHandler } from '../@core/events/application/handlers/my-handler.handler';
+import { ModuleRef } from '@nestjs/core';
+import { BullModule, InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
+import { IIntegrationEvent } from '../@core/common/domain/integration-event';
+import { PartnerCreatedIntegrationEvent } from '../@core/events/domain/events/integration-events/partner-created.int-events';
 
 @Module({
   imports: [
-    MikroOrmModule.forFeature({
-      entities: [
-        CustomerSchema,
-        PartnerSchema,
-        EventSchema,
-        EventSectionSchema,
-        EventSpotSchema,
-        OrderSchema,
-        SpotReservationSchema,
-      ],
+    MikroOrmModule.forFeature([
+      CustomerSchema,
+      PartnerSchema,
+      EventSchema,
+      EventSectionSchema,
+      EventSpotSchema,
+      OrderSchema,
+      SpotReservationSchema,
+    ]),
+    ApplicationModule,
+    BullModule.registerQueue({
+      name: 'integration-events',
     }),
   ],
   providers: [
@@ -75,35 +82,33 @@ import { OrdersController } from './orders/orders.controller';
     },
     {
       provide: PartnerService,
-      useFactory: (partnerRepo: IPartnerRepository, uow: IUnitOfWork) =>
-        new PartnerService(partnerRepo, uow),
-      inject: ['IPartnerRepository', 'IUnitOfWork'],
+      useFactory: (
+        partnerRepo: IPartnerRepository,
+        appService: ApplicationService,
+      ) => new PartnerService(partnerRepo, appService),
+      inject: ['IPartnerRepository', ApplicationService],
     },
     {
       provide: CustomerService,
-      useFactory: (customerRepo: ICustomerRepository, uow: IUnitOfWork) =>
-        new CustomerService(customerRepo, uow),
+      useFactory: (customerRepo, uow) => new CustomerService(customerRepo, uow),
       inject: ['ICustomerRepository', 'IUnitOfWork'],
     },
     {
       provide: EventService,
-      useFactory: (
-        eventRepo: IEventRepository,
-        partnerRepo: IPartnerRepository,
-        uow: IUnitOfWork,
-      ) => new EventService(eventRepo, partnerRepo, uow),
+      useFactory: (eventRepo, partnerRepo, uow) =>
+        new EventService(eventRepo, partnerRepo, uow),
       inject: ['IEventRepository', 'IPartnerRepository', 'IUnitOfWork'],
     },
     PaymentGateway,
     {
       provide: OrderService,
       useFactory: (
-        orderRepo: IOrderRepository,
-        customerRepo: ICustomerRepository,
-        eventRepo: IEventRepository,
-        spotReservationRepo: ISpotReservationRepository,
-        uow: IUnitOfWork,
-        paymentGateway: PaymentGateway,
+        orderRepo,
+        customerRepo,
+        eventRepo,
+        spotReservationRepo,
+        uow,
+        paymentGateway,
       ) =>
         new OrderService(
           orderRepo,
@@ -122,6 +127,14 @@ import { OrdersController } from './orders/orders.controller';
         PaymentGateway,
       ],
     },
+    {
+      provide: MyHandlerHandler,
+      useFactory: (
+        partnerRepo: IPartnerRepository,
+        domainEventManager: DomainEventManager,
+      ) => new MyHandlerHandler(partnerRepo, domainEventManager),
+      inject: ['IPartnerRepository', DomainEventManager],
+    },
   ],
   controllers: [
     PartnersController,
@@ -132,4 +145,30 @@ import { OrdersController } from './orders/orders.controller';
     OrdersController,
   ],
 })
-export class EventsModule {}
+export class EventsModule implements OnModuleInit {
+  constructor(
+    private readonly domainEventManager: DomainEventManager,
+    private moduleRef: ModuleRef,
+    @InjectQueue('integration-events')
+    private integrationEventsQueue: Queue<IIntegrationEvent>,
+  ) {}
+
+  onModuleInit() {
+    console.log('EventsModule initialized');
+    MyHandlerHandler.listensTo().forEach((eventName: string) => {
+      this.domainEventManager.register(eventName, async (event) => {
+        const handler: MyHandlerHandler =
+          await this.moduleRef.resolve(MyHandlerHandler);
+        await handler.handle(event);
+      });
+    });
+    this.domainEventManager.registerForIntegrationEvent(
+      PartnerCreated.name,
+      async (event) => {
+        console.log('integration events');
+        const integrationEvent = new PartnerCreatedIntegrationEvent(event);
+        await this.integrationEventsQueue.add(integrationEvent);
+      },
+    );
+  }
+}
